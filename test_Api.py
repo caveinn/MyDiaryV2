b54ac08691e138e33a11aa2ae8b726ecdea684bd
Kevin Nzioka <caveinncicad@gmail.com>
2018-07-24 16:00:59 +0300

    [ch #159267109] write test for adding an entry

--- test_Api.py
+++ test_Api.py
@@ -3,10 +3,11 @@ import json
 
 class apiTestCase(unittest.TestCase):
 	def setUp(self):
-		# self.app=createApp(configName="testing")
-		# self.client=self.app.test_client
+		self.app=createApp(configName="testing")
+		self.client=self.app.test_client
 		self.userInformation=json.dumps({"content-type":"application/json","name":"jane doe", "password":"12345", "email":"janedoe@gmail.com"})
-
+		self.entryInformation=jsonn.dumps({"cont-type":"application/json","title":"sample1", "content":"sample1 sample1 sample1 sample1"})
+	
 	def testLogin(self):
 		res=self.client().post("api/v2/auth/login",hearder={
 			"content-type":"application/json","authorizatin":"Basic amFuZSBkb2U6MTIzNA=="
@@ -24,3 +25,10 @@ class apiTestCase(unittest.TestCase):
 	def testGetSingleEntry(self):
 		res=self.client().get("api/v2/entries/1")
 		self.assertEqual(res.status_code,200)
+
+	def testAddEntry(self):
+		res=self.client().post("api/v2/entries", data =self.entryInformation)
+		self.assertEqual(res.status_code,201)
+
+if __name__=="__main__":
+	unittest.main()

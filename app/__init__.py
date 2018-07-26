6b7fd75dcfd80ed90a9c24d23026278d15a54b1c
Kevin Nzioka <caveinncicad@gmail.com>
2018-07-26 14:08:16 +0300

    [ft #159329969] create user table data model

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+'''create the app'''
 from flask import Flask
-from instance.config import appConfig
-
-def createApp(configName):
-	app=Flask("__name__")
-	app.config.from_object(appConfig[configName])
-	app.config.from_pyfile("instance/config.py")
+from instance.config import app_config
+from models import db_table,user
 
 
-	return app
+def create_app(configName):
+    app = Flask("__name__")
+    app.config.from_object(app_config[configName])
+    app.config.from_pyfile("instance/config.py")
+
+
+    return app
--- test_Api.py
+++ test_Api.py
@@ -1,39 +1,39 @@
+'''Test api endpoints'''
 import unittest
 import json
-from app import createApp
+from app import create_app
 
 
-class apiTestCase(unittest.TestCase):
+class api_test_case(unittest.TestCase):
     def setUp(self):
-	   self.app=createApp(configName="testing")
-	   self.client=self.app.test_client
-	   self.userInformation=json.dumps({"content-type": "application/json", "name": "jane doe", "password":"12345", "email":"janedoe@gmail.com"})
-	   self.entryInformation=json.dumps({"cont-type":"application/json","title":"sample1", "content":"sample1 sample1 sample1 sample1"})
-	
-    def testLogin(self):
-	   res=self.client().post("api/v2/auth/login", hearders={"content-type":"application/json","authorizatin":"Basic amFuZSBkb2U6MTIzNA=="})
-	   self.assertEqual(res.status_code,200)
+        self.app = create_app(configName="testing")
+        self.client = self.app.test_client
+        self.user_information = json.dumps({"content-type": "application/json", "name": "jane doe", "password":"12345", "email":"janedoe@gmail.com"})
+        self.entry_information = json.dumps({"cont-type":"application/json","title":"sample1", "content":"sample1 sample1 sample1 sample1"})
 
-    def testSignUp(self):
-	   res=self.client().post("api/v2/auth/signup",data=self.userInformation)
-	   self.assertEqual(res.status_code,201)
+    def test_login(self):
+        res = self.client().post("api/v2/auth/login", hearders={"content-type":"application/json","authorizatin":"Basic amFuZSBkb2U6MTIzNA=="})
+        self.assertEqual(res.status_code,200)
 
-    def testGetEntries(self):
-	   res=self.client().get("api/v2/entries")
-	   self.assertEqual(res.status_code,200)
+    def test_signup(self):
+       res=self.client().post("api/v2/auth/signup",data=self.userInformation)
+       self.assertEqual(res.status_code,201)
 
-    def testGetSingleEntry(self):
-	   res=self.client().get("api/v2/entries/1")
-	   self.assertEqual(res.status_code,200)
+    def test_get_entries(self):
+       res=self.client().get("api/v2/entries")
+       self.assertEqual(res.status_code,200)
 
-    def testAddEntry(self):
-	   res=self.client().post("api/v2/entries", data =self.entryInformation)
-	   self.assertEqual(res.status_code,201)
+    def test_get_single_entry(self):
+       res=self.client().get("api/v2/entries/1")
+       self.assertEqual(res.status_code,200)
 
-    def testModifyEntry(self):
-	   res=self.client().put("/api/v2/entries/1",data=self.entryInformation)
-	   self.assertEqual(res.status_code,200)
+    def test_add_entry(self):
+       res=self.client().post("api/v2/entries", data =self.entryInformation)
+       self.assertEqual(res.status_code,201)
 
-if __name__=="__main__":
-	unittest.main()
+    def test_modify_entry(self):
+       res=self.client().put("/api/v2/entries/1",data=self.entryInformation)
+       self.assertEqual(res.status_code,200)
 
+if __name__ == "__main__":
+    unittest.main()

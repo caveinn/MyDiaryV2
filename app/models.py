aebfa5b99b2bbe5f8fb0b6da87c396655435b4a8
Kevin Nzioka <caveinncicad@gmail.com>
2018-08-01 05:11:26 +0300

    [chore #159451461] refactor db class and user class

--- app/models.py
+++ app/models.py
@@ -1,59 +1,85 @@
 '''creating and managing database tables'''
 import psycopg2
+import os
 
-class db_table(object):
+class Db(object):
     '''class to initialise tables in db'''
-    def __init__(self, db_name):
-        self.db_name = db_name
+    def __init__(self, app):
+        self.db_name = app.config.get("DB_NAME")
+        self.db_host = app.config.get("DB_HOST")
+        self.db_password = app.config.get("DB_PASSWORD")
+        self.db_user = app.config.get("DB_USER")
+        self.conn = None
+        if os.getenv("APP_SETTINGS") == "testing" or os.getenv("APP_SETTINGS") == "development":
+            self.conn = psycopg2.connect(database=self.db_name)
+        else:
+            self.conn = psycopg2.connect(
+                database=self.db_name,
+                password=self.db_password,
+                user=self.db_user,
+                host=self.db_host
+                )
+        self.cursor = self.conn.cursor()
+
 
     def create_tables(self):
-        connection = psycopg2.connect(
-            database  = "d9uk0uj735e4rp",
-            password = '4fc5272295b21a8157ca9c88c95ebcf100f57112247356756dc725974b810a22',
-            user  = 'pfpewbzvzcrsnk',
-            host  = "ec2-54-204-23-228.compute-1.amazonaws.com",
-            )
-        cursor = connection.cursor()
-        #create users table
-        cursor.execute("CREATE TABLE users (id serial PRIMARY KEY, " \
-            "username varchar not null unique,password varchar not null )"
-                      )
-        #create entries db
-        cursor.execute("CREATE TABLE entries (id serial PRIMARY KEY, title varchar not null unique"\
-            ",content varchar not null, user_id integer references users(id))")
-        connection.commit()
-        connection.close()
+        try:
+            #create users table
+            self.cursor.execute(
+                "CREATE TABLE users (id serial PRIMARY KEY, " \
+                "username varchar not null unique,password varchar not null )"
+                )
+            #create entries db
+            self.cursor.execute(
+                "CREATE TABLE entries (id serial PRIMARY KEY, title varchar not null unique"\
+                ",content varchar not null, user_id integer references users(id))"
+                )
+        except psycopg2.Error as db_error:
+            return db_error.pgerror
+        self.conn.commit()
+    def get_all_users(self):
+        '''function to return all users'''
+        #save information to db
+        self.cursor.execute("SELECT * FROM users")
+        user_data = self.cursor.fetchall()
+        user_list = []
+        for single_user in user_data:
+            user_list.append(
+                User(id=single_user[0], username=single_user[1], email=single_user[2],
+                password=single_user[3], app=self.app)
+                )
+        return user_list
 
     def drop_all(self):
-        connection = psycopg2.connect(
-            database  = "d9uk0uj735e4rp",
-            password = '4fc5272295b21a8157ca9c88c95ebcf100f57112247356756dc725974b810a22',
-            user  = 'pfpewbzvzcrsnk',
-            host  = "ec2-54-204-23-228.compute-1.amazonaws.com",
-            )
-        cursor = connection.cursor()
-        cursor.execute("SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_schema,table_name")
-        rows = cursor.fetchall()
+        self.cursor.execute(
+            "SELECT table_schema, table_name FROM information_schema.tables WHERE"\
+             "table_schema = 'public' ORDER BY table_schema,table_name"
+        )
+        rows = self.cursor.fetchall()
         for row in rows:
-            cursor.execute("drop table "+row[1] + " cascade") 
-        cursor.close()
-        connection.commit()
-        connection.close()
+            self.cursor.execute("drop table "+row[1] + " cascade")
+        self.conn.commit()
 
 
-class user(object):
+class User(Db):
     '''class store users in db and perform various functions'''
-    def __init__(self, db_name):
-        '''inititalize empty user object'''
-        self.data = None
-        self.username = None
-        self.password = None
-        self.db_name = db_name
-
-    def create(self, name, password):
-        self.username = name
+    def __init__(self, app, username, email, password, user_id=None):
+        '''inititalize a user object'''
+        super(User, self).__init__(app)
+        self.username = username
         self.password = password
-        self.save()
+        self.email = email
+        self.user_id = user_id
+        user_list = self.get_all_users()
+        for user in user_list:
+            if user.username == username or user.email == email:
+                return {"mesage": "cannot store duplicate email or username"}
+        #save information to db
+        self.cursor.execute(
+            "INSERT INTO users (username, email password) VALUES (%s, %s, %s)",
+            (self.username, self.password, )
+            )
+        self.conn.commit()
 
     def save(self):
         '''save user after creation or modification'''
@@ -63,12 +89,7 @@ class user(object):
             user  = 'pfpewbzvzcrsnk',
             host  = "ec2-54-204-23-228.compute-1.amazonaws.com",
             )
-        cursor = connection.cursor()
-        #save information to db
-        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)",
-                       (self.username, self.password, )
-                      )
-        connection.commit()
+        
         connection.close()
 
     def get_all(self):
